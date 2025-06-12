0880751c0411a7479546382d9dd2c2f8c9351a81
Matouš Bohoněk <bohonmat@gm.gist.ac.kr>
2025-06-12 17:31:22 +0900

    Update event logging

--- src/events.cpp
+++ src/events.cpp
@@ -27,44 +27,50 @@ void ToolChangeEvent::playSound () const
 {
     if ( ! soundFile.empty () )
     {
-        cout << "Playing sound: " << soundFile << endl;
+        cout << "Playing sound: " << soundFile << endl << endl;
         playSoundSFML ( soundFile );
     }
 }
 
 void ToolChangeEvent::log () const
 {
+    cout << "-------- ToolChangeEvent BEGIN --------" << endl;
     cout << "Tool Change: " << toolName << ", Action: " << action << endl;
+    cout << "-------- ToolChangeEvent END --------" << endl << endl;
 }
 
 void GuardChangeEvent::playSound () const
 {
     if ( ! soundFile.empty () )
     {
-        cout << "Playing sound: " << soundFile << endl;
+        cout << "Playing sound: " << soundFile << endl << endl;
         playSoundSFML ( soundFile );
     }
 }
 
 void GuardChangeEvent::log () const
 {
+    cout << "-------- GuardChangeEvent BEGIN --------" << endl;
     cout << "Guard Change: " << guardName << ", Action: " << action << endl;
+    cout << "-------- GuardChangeEvent END --------" << endl << endl;
 }
 
 void GameStateLoadedEvent::playSound () const
 {
     if ( ! soundFile.empty () )
     {
-        cout << "Playing sound: " << soundFile << endl;
+        cout << "Playing sound: " << soundFile << endl << endl;
         playSoundSFML ( soundFile );
     }
 }
 
 void GameStateLoadedEvent::log () const
 {
+    cout << "-------- GameStateLoadedEvent BEGIN --------" << endl;
     cout << "Game State Loaded: " << endl;
     if ( state )
         state->print ();
     else
         cout << "No state available." << endl;
+    cout << "-------- GameStateLoadedEvent END --------" << endl << endl;
 }
